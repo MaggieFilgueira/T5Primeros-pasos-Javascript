00051baafac45556f1d7c06a67764af49b5e2290
Maggie <maggie.filgueira@gmail.com>
2023-06-07 12:54:08 +0200

    actualizacion

--- script.js
+++ script.js
@@ -1,36 +1,67 @@
 //VARIABLES
-let año = 1978
-let dias = 30
-let meses = 12
-let nombre = "Maggie"
-let casdo = true
-let ciudad = "Avilés"
-let one = "Hola, "
-let two = "como estas?"
-let joined = one + two
-console.log (joined)
-console.log (año)
-console.log (año+dias)
-console.log (meses*dias)
-console.log (8*meses*dias)
-console.log (ciudad + nombre)
-console.log("Hola " + nombre)
+//Crear variables de diferentes tipos de datos: número, cadena de texto, booleano e imprimir su valor por
+//la pantalla del navegador y la consola.
+let año = 1978;
+let dias = 30;
+let meses = 12;
+let nombre = " Maggie ";
+let casdo = true;
+let ciudad = " Avilés ";
+let one = "Hola, ";
+let two = "como estas?";
+let joined = one + two;
+console.log (joined);
+document.write (joined);
+console.log (año);
+document.write(año);
+console.log (año+dias);
+document.write(año+dias);
+console.log (meses*dias);
+document.write(meses*dias);
+console.log (8*meses*dias);
+document.write(8*meses*dias);
+console.log (ciudad + nombre);
+document.write(ciudad + nombre);
+console.log("Hola " + nombre);
+document.write("Hola " + nombre);
 
+//De las variables declaradas anteriormente, identifica las que son de tipo integer y realiza operaciones matemáticas básicas con las variables creadas anteriormente (suma, resta, multiplicación, división).
+//Deberás imprimir el resultado por la pantalla del navegador y la consola.
+//De las variables declaradas anteriormente, identifica las que son de tipo texto y concatena dichas cadenas de texto.Deberás imprimir el resultado por la pantalla del navegador y la consola.
 let num1 = 10;
 let num2 = 40;
 console.log (9 * num1);
+document.write(9 * num1);
 console.log (num2 / num1);
+document.write(num2 / num1);
 console.log (num2 + num1);
-
+document.write(num2 + num1);
 
 
     let ListaCompra = ["agua", "azucar", "limones", "fresas", "lechuga", "tomate", "pan", "manzanas", "guantes"];
 console.log(ListaCompra);
 
+//FUNCIONES
+//Crear una función que imprima por la pantalla del navegador y la consola el mensaje "Hola, [nombre]" con un parámetro para el nombre.
+
+function saludar(nombre) {
+  const saludo = "Hola," + nombre;
+  console.log(saludo);
+  document.write(saludo);
+  return saludo;
+}
+saludar("Maggie");
+
+//Crear función que reciba como parámetros dos números y que devuelva la suma de ellos. Deberás imprimir el resultado por la pantalla del navegador y la consola.
+
+function sumar(sum1, sum2)  {
+let resultado=num1+num2;
+console.log(resultado);
+document.write(resultado);
+return resultado;
+}
+sumar()
 //OBJETOS
-
-
-
 function Car(make, model, year, owner) {
   this.make = "Ford";
   this.model = "Mondeo";
